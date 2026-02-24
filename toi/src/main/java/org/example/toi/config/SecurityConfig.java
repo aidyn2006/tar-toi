b1b8aced375d34100fd94354b6a98d63c1caa74f
aidyn <a.nurlan@documentolog.com>
2026-02-24 16:22:58 +0500

    #tar: added Keyclok

---
+++ toi/src/main/java/org/example/toi/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.example.toi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(Customizer.withDefaults())
+                );
+
+        return http.build();
+    }
+}
