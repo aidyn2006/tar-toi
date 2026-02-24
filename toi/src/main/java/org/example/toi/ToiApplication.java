babc836aa5512b33841252caaad95c851e6f5528
aidyn <a.nurlan@documentolog.com>
2026-02-24 16:13:06 +0500

    Inital commit

---
+++ toi/src/main/java/org/example/toi/ToiApplication.java
@@ -0,0 +1,13 @@
+package org.example.toi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToiApplication.class, args);
+    }
+
+}
