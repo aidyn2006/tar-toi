7fe91c2c63f7bc0ce9fb08cc60d027289f9a9e4c
whosadik <adikmanmedet@gmail.com>
2026-03-17 11:46:00 +0500

    moved the categories to a separate service and made them working

---
+++ frontend/src/config/categories.js
@@ -0,0 +1,62 @@
+export const categories = [
+    {
+        id: 'uzatu',
+        title: {
+            kk: 'Ұзату',
+            ru: 'Проводы невесты',
+        },
+        icon: '✨',
+        count: 4,
+        bg: '#f0fdf4',
+    },
+    {
+        id: 'wedding',
+        title: {
+            kk: 'Үйлену тойы',
+            ru: 'Свадьба',
+        },
+        icon: '💍',
+        count: 9,
+        bg: '#fffbeb',
+    },
+    {
+        id: 'sundet',
+        title: {
+            kk: 'Сүндет той',
+            ru: 'Сүндет той',
+        },
+        icon: '👦',
+        count: 3,
+        bg: '#f0fdf4',
+    },
+    {
+        id: 'tusaukeser',
+        title: {
+            kk: 'Тұсаукесер',
+            ru: 'Тұсаукесер',
+        },
+        icon: '👣',
+        count: 4,
+        bg: '#fffbeb',
+    },
+    {
+        id: 'merei',
+        title: {
+            kk: 'Мерейтой',
+            ru: 'Юбилей',
+        },
+        icon: '🎂',
+        count: 3,
+        bg: '#f0fdf4',
+    },
+    {
+        id: 'besik',
+        title: {
+            kk: 'Бесік той',
+            ru: 'Бесік той',
+        },
+        icon: '👶',
+        count: 3,
+        bg: '#fffbeb',
+    },
+];
